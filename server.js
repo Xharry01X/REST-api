8986b0347b59327d4ec0a90de6d8468cbfb6b196
harshit-506 <supriyamohapatra658@gmail.com>
2024-06-06 12:26:19 +0530

    time to remove all this

--- server.js
+++ server.js
@@ -2,6 +2,25 @@ import express from "express"
 const app=express()
 const port=4000
 
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose.connect('mongodb://root:example@mongo:27017/mydatabase', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('Connected to MongoDB');
+}).catch(err => {
+    console.error('Could not connect to MongoDB', err);
+});
+
+// Sample route
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
 app.listen(port,()=>{
     console.log(`Server live at ${port}`)
 }) 
